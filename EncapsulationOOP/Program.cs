17bd9444c2dd794335a85faa5c8d70d04ff32b4e
kukita123 <kukita@abv.bg>
2026-05-04 14:50:38 +0300

    added class Point, class SalesPerson

--- EncapsulationOOP/Program.cs
+++ EncapsulationOOP/Program.cs
@@ -13,6 +13,12 @@ namespace EncapsulationOOP
                 Console.WriteLine("The Fastets Car Is {0} {1}", 
                     car2.Brand, car2.Model);
         }
+
+        static double DistanceBetweenPoints(Point first, Point second)
+        {
+            return Math.Sqrt(Math.Pow(first.X - second.X, 2) + Math.Pow(first.Y - second.Y, 2));
+        }
+
         static void Main(string[] args)
         {
             #region 9a
@@ -40,9 +46,68 @@ namespace EncapsulationOOP
             //car3.MaxSpeed = 181;
             //CompareCars(car2, car3);
 
+            ////да се създаде масив от 5 коли, да се зареди
+            //// със стойности и да се приложи сравнение на
+            //// всяка кола с останалите
+            //Car[] cars = new Car[5];
+            //for (int i = 0; i < cars.Length; i++)
+            //{
+            //    cars[i] = new Car();
+            //}
+            //for (int i = 0; i < cars.Length; i++)
+            //{
+            //    Console.WriteLine("Enter Brand - car number [{0}]: ", i);
+            //    cars[i].Brand = Console.ReadLine();
+            //    Console.WriteLine("Enter Model - car number [{0}]: ", i);
+            //    cars[i].Model = Console.ReadLine();
+            //    Console.WriteLine("Enter Seats - car number [{0}]: ", i);
+            //    cars[i].Seats = int.Parse(Console.ReadLine());
+            //    Console.WriteLine("Enter Max Speed - car number [{0}]: ", i);
+            //    cars[i].MaxSpeed = int.Parse(Console.ReadLine());
+            //}
+            //Console.WriteLine(cars[2].MaxSpeed);
+            //for (int i = 0; i < cars.Length - 1; i++)
+            //{
+            //    for (int j = 0; j < cars.Length; j++)
+            //    {
+            //        CompareCars(cars[i], cars[j]);
+            //    }
+            //}
             #endregion
-            #endregion
+            #region Point
+            Point p1 = new Point();
+            Point p2 = new Point(2, 3);
+            Console.WriteLine(p1.DistanceToPoint(p2));
+            Console.WriteLine(p2.DistanceToPoint(p1));
+            Console.WriteLine(DistanceBetweenPoints(p1, p2));
 
+            Random r = new Random();
+            int n = r.Next(5, 10);
+
+            Point[] points = new Point[n];
+
+            for (int i = 0; i < points.Length; i++)
+            {
+                points[i] = new Point(r.Next(-10, 10), r.Next(-10, 10));
+                Console.WriteLine("points[{0}] coordinates: ({1}, {2})", i, points[i].X, points[i].Y);
+            }
+            Console.WriteLine();
+
+            double min = DistanceBetweenPoints(points[0], points[1]);
+            for (int i = 0; i < points.Length - 1; i++)
+            {
+                for (int j = i + 1; j < points.Length; j++)
+                {
+                    double newDistance = DistanceBetweenPoints(points[i], points[j]);
+                    if (newDistance < min)
+                        min = newDistance;
+                }
+            }
+            Console.WriteLine("Min distance between points is: {0}", min);
+
+            Console.WriteLine();
+            #endregion
+            #endregion
             #region 9b
             #region Triangle
             //Triangle t1 = new Triangle();
@@ -59,41 +124,58 @@ namespace EncapsulationOOP
             //Console.WriteLine(t2.Perimeter());
             #endregion
             #region Car
-            Car car1 = new Car();
-            Car car2 = new Car("Fiat", "500e", 4, 180);
-            Car car3 = new Car("BMW", "E46", 5, 266);
+            //Car car1 = new Car();
+            //Car car2 = new Car("Fiat", "500e", 4, 180);
+            //Car car3 = new Car("BMW", "E46", 5, 266);
 
-            if (car1.MaxSpeed > car2.MaxSpeed)
-                Console.WriteLine("Fastest car is {0} {1}", car1.Brand, car1.Model);
-            else
-                Console.WriteLine("Fastest car is {0} {1}", car2.Brand, car2.Model);
+            //if (car1.MaxSpeed > car2.MaxSpeed)
+            //    Console.WriteLine("Fastest car is {0} {1}", car1.Brand, car1.Model);
+            //else
+            //    Console.WriteLine("Fastest car is {0} {1}", car2.Brand, car2.Model);
 
-            if (car1.MaxSpeed > car3.MaxSpeed)
-                Console.WriteLine("Fastest car is {0} {1}", car1.Brand, car1.Model);
-            else
-                Console.WriteLine("Fastest car is {0} {1}", car3.Brand, car3.Model);
+            //if (car1.MaxSpeed > car3.MaxSpeed)
+            //    Console.WriteLine("Fastest car is {0} {1}", car1.Brand, car1.Model);
+            //else
+            //    Console.WriteLine("Fastest car is {0} {1}", car3.Brand, car3.Model);
 
-            //objects array:
-            Car[] cars = new Car[5];
-            //constructor for every object in the array cars
-            for (int i = 0; i < cars.Length; i++)
-            {
-                cars[i] = new Car();
-            }
-            for (int i = 0; i < cars.Length; i++)
-            {
-                Console.Write("Enter Brand: ");
-                cars[i].Brand = Console.ReadLine();
-                Console.Write("Enter Model: ");
-                cars[i].Model = Console.ReadLine();
-                Console.Write("Enter Seats: ");
-                cars[i].Seats = int.Parse(Console.ReadLine());
-                Console.Write("Enter Max Speed: ");
-                cars[i].MaxSpeed = int.Parse(Console.ReadLine());
+            ////да се създаде масив от 5 коли, да се зареди
+            //// със стойности и да се приложи сравнение на
+            //// всяка кола с останалите
 
-            }
-            Console.WriteLine(cars[0].MaxSpeed);
-            Console.WriteLine(cars[3].MaxSpeed);
+            ////objects array:
+            //Car[] cars = new Car[5];
+            ////constructor for every object in the array cars
+            //for (int i = 0; i < cars.Length; i++)
+            //{
+            //    cars[i] = new Car();
+            //}
+            //for (int i = 0; i < cars.Length; i++)
+            //{
+            //    Console.Write("Enter Brand: ");
+            //    cars[i].Brand = Console.ReadLine();
+            //    Console.Write("Enter Model: ");
+            //    cars[i].Model = Console.ReadLine();
+            //    Console.Write("Enter Seats: ");
+            //    cars[i].Seats = int.Parse(Console.ReadLine());
+            //    Console.Write("Enter Max Speed: ");
+            //    cars[i].MaxSpeed = int.Parse(Console.ReadLine());
+
+            //}
+            //Console.WriteLine(cars[0].MaxSpeed);
+            //Console.WriteLine(cars[3].MaxSpeed);
+
+            ////CompareCars(cars[0], cars[1]);
+            //for (int i = 0; i < cars.Length - 1; i++)
+            //{
+            //    for (int j = i + 1; j < cars.Length; j++)
+            //    {
+            //        CompareCars(cars[i], cars[j]);
+            //    }
+            //}
+            #endregion
+            #region Point
+            //Point[] points = new Point[10];
+
             #endregion
             #endregion
 
---
+++ EncapsulationOOP/SalesPerson.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+Да се състави програма, описваща клас SalesPerson (продавач). 
+Данните на класа са: име и фамилия на продавач, месечните продажби в 
+продължение на една година (масив от 12 елемента от тип double). 
+Добавете валидация на данните - името и фамилията на продавача да съдържат 
+поне 3 символа.
+Методите на класа включват въвеждане и извеждане на данни за обектите от класа, 
+намиране на общата и средната сума на продажбите за 12-те месеца, конструктори. 
+Тествайте дефинираните методи, като създадете поне два обекта от класа и 
+сравните техните средномесечни продажби*/
+namespace EncapsulationOOP
+{
+    class SalesPerson
+    {
+        private string _name;
+        private string _family;
+        private double[] _sales;
+        public SalesPerson()
+        {
+            this._name = "";
+            this._family = "";
+            _sales = new double[12]; //!!!!!!!!!!!!!
+        }
+        public SalesPerson(string name, string family)
+        {
+            this._name = name;
+            this._family = family;
+            _sales = new double[12];
+        }
+
+        public string Name
+        {
+            get { return this._name; }
+            set { this._name = value; }
+        }
+        public string Family
+        {
+            get { return this._family; }
+            set { this._family = value; }
+        }
+        public double[] Sales
+        {
+            get { return this._sales; }
+            set
+            {
+                for (int i = 0; i < 12; i++)
+                {
+                    if (value[i] > 0)
+                        this._sales[i] = value[i];
+                }
+            }
+        }
+    }
+}
